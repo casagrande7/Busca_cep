1c1e1502e6197544b204f5f1d3c424eae462dd23
casagrande7 <joaofootz10@gmail.com>
2023-09-21 10:38:02 -0300

    Atualizado

--- src/components/BuscaCepForm.tsx
+++ src/components/BuscaCepForm.tsx
@@ -1,6 +1,8 @@
 import React, { useState, ChangeEvent, FormEvent, useEffect} from "react";
 
 import styles from "../App.module.css"
+import Footer from "./Footer";
+import Header from "./Header";
 
 const BuscaCep = () =>{
 
@@ -38,6 +40,8 @@ const BuscaCep = () =>{
 
     return (
         <div>
+            <Header/>
+            <main className={styles.main}>
             <form onSubmit={findCep}>
                 <label htmlFor="cep">CEP</label>
                 <input type="text" name="cep" id="cep" onChange={submitForm} />
@@ -47,6 +51,8 @@ const BuscaCep = () =>{
             <p>UF: {uf}</p>
             <p>CEP: {cep}</p>
             <p className={styles.error}>{erro}</p>
+        </main>
+            <Footer/>
         </div>
     );
 }
