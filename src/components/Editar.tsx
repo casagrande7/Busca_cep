5c4e50b5e3a97cb343e58ba7c0610f9c876c7abd
casagrande7 <joaofootz10@gmail.com>
2023-11-07 07:54:02 -0300

    Atualizado

---
+++ src/components/Editar.tsx
@@ -0,0 +1,12 @@
+import React, { Component, useState, ChangeEvent, FormEvent, useEffect } from "react";
+
+
+const Editar = () => {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default Editar;
