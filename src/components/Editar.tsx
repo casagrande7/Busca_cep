cc46c9c26f0c14b71c45fdffd91410a4da5a9c56
casagrande7 <joaofootz10@gmail.com>
2023-11-07 08:09:36 -0300

    Atualizar

--- src/components/Editar.tsx
+++ src/components/Editar.tsx
@@ -1,9 +1,77 @@
 import React, { Component, useState, ChangeEvent, FormEvent, useEffect } from "react";
+import styles from "../App.module.css";
+import Header from "./Header";
+import Footer from "./Footer";
 
 
 const Editar = () => {
+
+    const [nome, setNome] = useState<string>("");
+    const [email, setEmail] = useState<string>("");
+    const [cpf, setCpf] = useState<string>("");
+    const [id, setId] = useState<number>();
+
+    const atualizar = (e: FormEvent) => {
+
+    }
+
+    useEffect(() => {
+
+    }, []);
+
+    const handleState = (e: ChangeEvent<HTMLInputElement>) => {
+        if (e.target.name === "nome") {
+            setNome(e.target.value);
+        }
+        if (e.target.name === "cpf") {
+            setCpf(e.target.value);
+        }
+        if (e.target.name === "email") {
+            setEmail(e.target.value);
+        }
+    }
+
+
+
+
+
+
+
+
     return (
         <div>
+            <Header />
+            <main className={styles.main}>
+                <div className='container'>
+                    <div className='card'>
+                        <div className='card-body'>
+                            <h5 className='card-title'>Cadastrar Clientes</h5>
+                            <form onSubmit={atualizar} className='row g-3'>
+                                <div className='col-6'>
+                                    <label htmlFor='nome' className='form-label'>Nome</label>
+                                    <input type="text" name='nome' className='form-control' required onChange={handleState} />
+                                </div>
+                                <div className='col-6'>
+                                    <label htmlFor='email' className='form-label'>E-mail</label>
+                                    <input type="text" name='email' className='form-control' required onChange={handleState} />
+
+                                </div>
+                                <div className='col-6'>
+                                    <label htmlFor='cpf' className='form-label'>CPF</label>
+                                    <input type="text" name='cpf' className='form-control' required onChange={handleState} />
+
+                                </div>
+
+                                <div className='col-12'>
+                                    <button type='submit' className='btn btn-success btn-sn'>Atualizar</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </main>
+
+            <Footer />
 
         </div>
     );
